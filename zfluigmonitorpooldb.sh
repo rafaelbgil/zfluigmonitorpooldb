f30593bc07c212d47c027224bcc86c4a1b62c191
Rafael Benites Gil <rafaelbgil@users.noreply.github.com>
2016-03-02 14:03:59 -0300

    Update zfluigmonitorpooldb.sh

--- zfluigmonitorpooldb.sh
+++ zfluigmonitorpooldb.sh
@@ -3,7 +3,7 @@
 #Autor: Rafael Benites Gil
 
 JBOSS_CLI="/app/fluig/jboss/bin/jboss-cli.sh"
-IP_JBOSS="172.16.102.224"
+IP_JBOSS="127.0.0.1"
 PORTA_JBOSS="9999"
 JAVA_HOME="/app/fluig/jdk-64/"
 PATH=$JAVA_HOME/bin:$PATH
@@ -31,5 +31,4 @@ case $1 in
 esac
 		
 retorno=$(sh $JBOSS_CLI --connect --controller=$IP_JBOSS:$PORTA_JBOSS --commands="/subsystem=datasources/data-source=$DATASOURCE:read-resource(recursive=true,include-runtime=true)" | grep ActiveCount | grep -o "[0-9]*")
-#echo "Fila: $fila , Mensagens: $retorno"
 echo $retorno
