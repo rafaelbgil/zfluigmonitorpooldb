a4da818521f65ccacb9f6718641fc9e0cfabb084
Rafael Benites Gil <rafaelbgil@users.noreply.github.com>
2016-03-02 14:05:16 -0300

    Update zfluigmonitorpooldb.sh

--- zfluigmonitorpooldb.sh
+++ zfluigmonitorpooldb.sh
@@ -2,10 +2,10 @@
 #Monitoramento Pool Banco de Dados
 #Autor: Rafael Benites Gil
 
-JBOSS_CLI="/app/fluig/jboss/bin/jboss-cli.sh"
-IP_JBOSS="127.0.0.1"
-PORTA_JBOSS="9999"
-JAVA_HOME="/app/fluig/jdk-64/"
+JBOSS_CLI="/app/fluig/jboss/bin/jboss-cli.sh" # Informar onde se encontra o executavel jboss-cli.sh, recomendacao: informar o executavel que fica dentro do diretorio de instalacao do jboss do fluig, ex: /fluig/jboss/bin/jboss-cli.sh .
+IP_JBOSS="127.0.0.1" # Informar o ip da interface de gerenciamento do jboss.
+PORTA_JBOSS="9999" # Informar porta de gerenciamento do jboss.
+JAVA_HOME="/app/fluig/jdk-64/" # Informar o diretorio da instalacao do java, recomendacao: informar o diretorio jdk que fica dentro do diretorio de instalacao do fluig, ex: /fluig/jdk-64 .
 PATH=$JAVA_HOME/bin:$PATH
 export PATH JAVA_HOME
 case $1 in	
